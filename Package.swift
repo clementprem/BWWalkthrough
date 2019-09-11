239f2c9d270ba192ac0cbdee320d40a90649008e
Clement Premnath <clement@lifesum.com>
2019-09-11 14:00:05 +0200

    added swift package file

---
+++ Package.swift
@@ -0,0 +1,16 @@
+// swift-tools-version:5.0
+	import PackageDescription
+	
+	let package = Package(
+	    name: "BWWalkthrough",
+	    platforms: [.iOS(.v10)],
+	    products: [
+	        .library(name: "BWWalkthrough", type: .dynamic, targets: ["BWWalkthrough"])
+	    ],
+	    targets: [
+	        .target(
+	            name: "BWWalkthrough",
+	            path: "BWWalkthrough"
+	        )
+	    ]
+	)
